d2b71e30cf6282f310c404d20cfc36a228450231
João Carlos <joaocarlos.cic1@gmail.com>
2024-03-31 14:24:58 -0300

    feat: implementado servidor

---
+++ src/main/java/servidor/Servidor.java
@@ -0,0 +1,25 @@
+package servidor;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Servidor {
+    public static void main(String[] args) {
+
+        try {
+            Registry registry = LocateRegistry.createRegistry(1099);
+            System.out.println("RMI Registry pronto!");
+
+            Jogo jogo = new Jogo();
+
+            registry.rebind("QuatroEmLinha", jogo);
+            System.out.println("Servidor do jogo pronto!");
+        } catch (RemoteException e) {
+            System.out.println("Falha no servidor!");
+        }
+
+    }
+}
